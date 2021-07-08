fcb2839eb44c25b954006375f37d49815b0136c1
dooderek <84983797+dooderek@users.noreply.github.com>
2021-07-08 14:10:49 -0500

    Update fetchlogdeux.js

--- fetchlogdeux.js
+++ fetchlogdeux.js
@@ -1,13 +1,4 @@
 
 function addOrder(){
-fetch('/api/storefront/cart?include=lineItems.digitalItems.options,lineItems.physicalItems.options', {
-  credentials: 'same-origin'}
-     )
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
-  }); 
-    
+console.log("why hello")
 }
