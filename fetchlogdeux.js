d8abc75db7762498af81313ee4bdf92ce4d922a6
dooderek <84983797+dooderek@users.noreply.github.com>
2021-07-08 13:53:50 -0500

    Add files via upload

---
+++ fetchlogdeux.js
@@ -0,0 +1,13 @@
+
+function addOrder(){
+fetch('/api/storefront/cart?include=lineItems.digitalItems.options,lineItems.physicalItems.options', {
+  credentials: 'same-origin'}
+     )
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+  }); 
+    
+}
